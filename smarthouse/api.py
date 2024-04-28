aad77de1494f60eedb1711b82f46eca216abd7bb
Jo Martin Slåtten <jomartinslaatten98@hotmail.com>
2024-04-28 11:41:41 +0200

    Update actuator state update query in persistence.py

--- smarthouse/api.py
+++ smarthouse/api.py
@@ -1,14 +1,16 @@
-from datetime import datetime
-from random import random
+from typing import Literal
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
+from smarthouse.domain import Actuator, ActuatorWithSensor, Device, Floor, Measurement, Room, Sensor, SmartHouse
 from smarthouse.persistence import SmartHouseRepository
+from pydantic import BaseModel
 from pathlib import Path
 
 def setup_database():
-    project_dir = Path(__file__).parent.parent
+    project_dir = Path("data\db.sql").parent.parent
     db_file = project_dir / "data" / "db.sql" # you have to adjust this if you have changed the file name of the database
     print(db_file.absolute())
     return SmartHouseRepository(str(db_file.absolute()))
@@ -18,6 +20,93 @@ app = FastAPI()
 repo = setup_database()
 
 smarthouse = repo.load_smarthouse_deep()
+
+# Data Transfer Object definitions below
+class SmartHouseInfo(BaseModel):
+    no_rooms: int
+    no_floors: int 
+    total_area: float
+    no_devices: int 
+
+
+    @staticmethod
+    def from_obj(house: SmartHouse):
+        return SmartHouseInfo(
+            no_rooms=len(house.get_rooms()),
+            no_floors=len(house.get_floors()),
+            total_area=house.get_area(),
+            no_devices=len(house.get_devices()))
+
+
+class FloorInfo(BaseModel):
+    fid: int
+    rooms: list[int]
+
+    @staticmethod
+    def from_obj(floor: Floor):
+        return FloorInfo(
+            fid=floor.level,
+            rooms=[r.db_id for r in floor.rooms if r.db_id ]
+        )
+
+class RoomInfo(BaseModel):
+    rid: int | None
+    room_size: float
+    room_name: str | None
+    floor: int 
+    devices: list[str]
+
+    @staticmethod
+    def from_obj(room: Room):
+        return RoomInfo(
+            rid=room.db_id,
+            room_size=room.room_size,
+            floor=room.floor.level,
+            room_name=room.room_name,
+            devices=[d.id for d in room.devices]
+        )
+
+class DeviceInfo(BaseModel):
+    id: str
+    model: str
+    supplier: str
+    device_type: str
+    device_category: Literal["actuator"] | Literal["sensor"] | Literal["actuator_with_sensor"] | Literal["unknown"]
+    room: int | None
+
+
+    @staticmethod
+    def from_obj(device: Device):
+        category : Literal['actuator', 'sensor', 'actuator_with_sensor', 'unknown'] = "unknown"
+        if isinstance(device, ActuatorWithSensor):
+            category = "actuator_with_sensor"
+        elif isinstance(device, Actuator):
+            category = "actuator"
+        elif isinstance(device, Sensor):
+            category = "sensor"
+        return DeviceInfo(
+            id=device.id,
+            model=device.model_name,
+            supplier=device.supplier,
+            device_type=device.device_type,
+            device_category=category,
+            room=device.room.db_id if device.room else None
+        )
+
+class ActuatorStateInfo(BaseModel):
+    state: str | float
+
+    @staticmethod
+    def from_obj(actuator: Actuator):
+        if actuator.state and isinstance(actuator.state, float):
+            return ActuatorStateInfo(state=actuator.state)
+        elif actuator.state:
+            return ActuatorStateInfo(state="running")
+        else:
+            return ActuatorStateInfo(state="off")
+
+
+
 
 # http://localhost:8000/welcome/index.html
 app.mount("/static", StaticFiles(directory="www"), name="static")
@@ -35,83 +124,65 @@ def hello(name: str = "world"):
     return {"hello": name}
 
 
-# Starting point ...
-
 @app.get("/smarthouse")
-def get_smarthouse_info() -> dict[str, int | float]:
+def get_smarthouse_info() -> SmartHouseInfo:
     """
     This endpoint returns an object that provides information
     about the general structure of the smarthouse.
     """
-    return {
-        "no_rooms": len(smarthouse.get_rooms()),
-        "no_floors": len(smarthouse.get_floors()),
-        "registered_devices": len(smarthouse.get_devices()),
-        "area": smarthouse.get_area()
-    }
-
-# TODO: implement the remaining HTTP endpoints as requested in
-# https://github.com/selabhvl/ing301-projectpartC-startcode?tab=readme-ov-file#oppgavebeskrivelse
-# here ...
+    return SmartHouseInfo.from_obj(smarthouse)
 
 
 @app.get("/smarthouse/floor")
-def get_floors() -> dict[str, int | float | str]:
-    info = {}
-    floors = smarthouse.get_floors()  # Antar denne metoden returnerer en liste med etasjer
-    for i, floor in enumerate(floors):
-        key = f"floor_no {i+1}, no of rooms "
-        info[key] = len(floor.rooms)
-
-    return info
+def get_floors() -> list[FloorInfo]:
+    return [FloorInfo.from_obj(x) for x in smarthouse.get_floors()]
 
 
 @app.get("/smarthouse/floor/{fid}")
-def get_floor(fid: int) -> dict[str, int | float | str]:
-    floor = smarthouse.get_floors()[fid-1]
-    return {
-        "floor_no": fid,
-        "no_rooms": len(floor.rooms),
-    }
+def get_floor(fid: int) -> Response:
+    for f in smarthouse.get_floors():
+        if f.level == fid:
+            return JSONResponse(content=jsonable_encoder(FloorInfo.from_obj(f)))
+    return Response(status_code=404)
 
 
 @app.get("/smarthouse/floor/{fid}/room")
-def get_rooms(fid: int) -> dict[str, int | float | str]:
-    floor = smarthouse.get_floors()[fid-1]
-    rooms = floor.rooms
-    info = {}
-    for i, room in enumerate(rooms):
-        key = f"room_no {i+1}, room name {room.room_name}, area "
-        info[key] = room.room_size
-    return info
+def get_rooms(fid: int) -> list[RoomInfo]:
+    return [RoomInfo.from_obj(r) for r in smarthouse.get_rooms() if r.floor.level == fid]
 
 
 @app.get("/smarthouse/floor/{fid}/room/{rid}")
-def get_room(fid: int, rid: int) -> dict[str, int | float | str]:
-    floor = smarthouse.get_floors()[fid-1]
-    room = floor.rooms[rid-1]
-    return {
-        "room_name": room.room_name,
-        "room_no": rid,
-        "area": room.room_size,
-    }
+def get_room(fid: int, rid: int) -> Response:
+    for r in smarthouse.get_rooms():
+        if r.db_id == rid and r.floor.level == fid:
+            return JSONResponse(content=jsonable_encoder(RoomInfo.from_obj(r)))
+    return Response(status_code=404)
 
 
 @app.get("/smarthouse/device")
-def get_devices():
-    info = []
-    for i in range(len(smarthouse.get_devices())):
-        info.append({"device": i+1, "room": smarthouse.get_devices()[i].room.room_name})
+def get_devices() -> list[DeviceInfo]:
+    return [DeviceInfo.from_obj(d) for d in smarthouse.get_devices()]
 
-    return info
 
 @app.get("/smarthouse/device/{uuid}")
-def get_device(uuid: str):
-    for i in range(len(smarthouse.get_devices())):
-        if smarthouse.get_devices()[i].id == uuid:
-            return {"device": i+1, "room": smarthouse.get_devices()[i].room.room_name}
+def get_device(uuid: str) -> Response:
+    for d in smarthouse.get_devices():
+        if d.id == uuid:
+            return JSONResponse(content=jsonable_encoder(DeviceInfo.from_obj(d)))
+    return Response(status_code=404)
 
-    return {"error": "device not found"}
+
+@app.get("/smarthouse/sensor/{uuid}/current")
+def get_most_recent_measurement(uuid: str) -> Response:
+    device = smarthouse.get_device_by_id(uuid)
+    if device and device.is_sensor():
+        reading = repo.get_latest_reading(device)
+        if reading:
+            return JSONResponse(content=jsonable_encoder(reading))
+        else:
+            return JSONResponse(content=jsonable_encoder({'reason': 'no timeseries available'}), status_code=404)
+    else:
+        return JSONResponse(content=jsonable_encoder({'reason': 'sensor with id not found'}), status_code=404)
 
 
 @app.get("/smarthouse/sensor/{uuid}/current")
@@ -119,65 +190,51 @@ def get_sensor(uuid: str):
     for i in range(len(smarthouse.get_devices())):
         if smarthouse.get_devices()[i].id == uuid:
             return {"device": i+1, "room": smarthouse.get_devices()[i].room.room_name, "temperature": repo.get_latest_reading(smarthouse.get_devices()[i])}
-        
+
     return {"error": "device not found"}
-
-
-@app.post("/smarthouse/sensor/{uuid}/current/{measurement}")
-def post_sensor(uuid: str, measurement: float):
-    for i in range(len(smarthouse.get_devices())):
-        if smarthouse.get_devices()[i].id == uuid:
-            c = repo.conn.cursor()
-            c.execute(f"INSERT INTO measurements (device, ts, value, unit) VALUES ('{uuid}', '{datetime.now().isoformat()}', {measurement}, '{smarthouse.get_devices()[i].unit}');")
-            repo.conn.commit()
-            c.close()
 
 
 @app.get("/smarthouse/sensor/{uuid}/values")
-def get_sensor_values(uuid: str, limit: int):
-
-    if limit:
-        c = repo.conn.cursor()
-        c.execute(f"SELECT ts, value, unit FROM measurements WHERE device = '{uuid}' ORDER BY ts DESC LIMIT {limit};")
+def get_measurements(uuid: str, n: int | None = None) -> Response:
+    device = smarthouse.get_device_by_id(uuid)
+    if device and device.is_sensor():
+        result = repo.get_readings(uuid, n)
+        return JSONResponse(content=jsonable_encoder(result), status_code=200)
     else:
-        c = repo.conn.cursor()
-        c.execute(f"SELECT ts, value, unit FROM measurements WHERE device = '{uuid}';")
-    result = c.fetchall()
-    c.close()
-    return result
+        return JSONResponse(content=jsonable_encoder({'reason': 'sensor with uuid not found'}), status_code=404)
 
 
 @app.delete("/smarthouse/sensor/{uuid}/oldest")
-def delete_sensor_values(uuid: str):
-    c = repo.conn.cursor()
-    c.execute(f"SELECT ts FROM measurements WHERE device = '{uuid}' ORDER BY ts ASC LIMIT 1;")
-    ts_for_oldest_measurement = c.fetchone()[0]
-    c.execute(f"DELETE FROM measurements WHERE device = '{uuid}' AND ts = '{ts_for_oldest_measurement}';")
-    repo.conn.commit()
-    c.close()
+def delete_old_measurement(uuid: str) -> Response:
+    device = smarthouse.get_device_by_id(uuid)
+    if device and device.is_sensor():
+        result = repo.delete_oldest_reading(uuid)
+        return JSONResponse(content=jsonable_encoder(result), status_code=200)
+    else:
+        return JSONResponse(content=jsonable_encoder({'reason': 'sensor with uuid not found'}), status_code=404)
+
 
 
 @app.get("/smarthouse/actuator/{uuid}/current")
-def get_actuator(uuid: str):
-    for i in range(len(smarthouse.get_devices())):
-        if smarthouse.get_devices()[i].id == uuid:
-            c = repo.conn.cursor()
-            c.execute(f"SELECT state FROM actuator_state WHERE device_id = '{uuid}';")
-            state = c.fetchone()
-            c.close()
-            return {"device": i+1, "room": smarthouse.get_devices()[i].room.room_name, "state": state}
-
-    return {"error": "device not found"}
+def get_sensor_state(uuid: str) -> Response:
+    device = smarthouse.get_device_by_id(uuid)
+    if device and isinstance(device, Actuator):
+        return JSONResponse(jsonable_encoder(ActuatorStateInfo.from_obj(device)))
+    else:
+        return JSONResponse(content=jsonable_encoder({'reason': 'actuator with uuid not found'}), status_code=404)
 
 
-@app.put("/smarthouse/device/{uuid}")
+@app.put("/smarthouse/actuator/{uuid}/")
 def put_device(uuid: str):
     for i in range(len(smarthouse.get_devices())):
         if smarthouse.get_devices()[i].id == uuid:
+            
             repo.update_actuator_state(smarthouse.get_devices()[i])
-            return {smarthouse.get_devices()[i].state}
+            return {"device": i+1, "room": smarthouse.get_devices()[i].room.room_name, "state": smarthouse.get_devices()[i].state}
 
     return {"error": "device not found"}
+    
+
 
 
 if __name__ == '__main__':
